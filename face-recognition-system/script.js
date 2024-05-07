f2c7293601828fa047a58bedf21973f83cd884fe
red-straw <144910907+red-straw@users.noreply.github.com>
2024-05-08 03:16:26 +0900

    Add files via upload

---
+++ face-recognition-system/script.js
@@ -0,0 +1,59 @@
+const video = document.getElementById('video');
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models'), // 감정 인식 모델 로드
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+]).then(startVideo).catch(err => console.error(err));
+
+async function startVideo() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
+    video.srcObject = stream;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+video.addEventListener('play', async () => {
+  const labeledFaceDescriptors = await loadLabeledImages();
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors().withFaceExpressions(); // 감정 정보도 함께 가져오기
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections); // 감정 정보 표시
+
+    resizedDetections.forEach(async detection => {
+      const bestMatch = faceMatcher.findBestMatch(detection.descriptor);
+      const box = detection.detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, { label: bestMatch.toString() });
+      drawBox.draw(canvas);
+    });
+  }, 100);
+});
+
+async function loadLabeledImages() {
+  const labels = ['임민섭', '장충희', '이지호', '김혁'];
+  return Promise.all(
+    labels.map(async label => {
+      const descriptions = [];
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(`/labeled_images/${label}/${i}.jpg`);
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
+        descriptions.push(detections.descriptor);
+      }
+
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
+    })
+  );
+}
